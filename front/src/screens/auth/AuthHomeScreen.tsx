3c0557247be2c04d4bd1693f7c25794d1d6f0831
chosule <89799325+chosule@users.noreply.github.com>
2025-04-09 21:59:18 +0900

    CustomButton apply / login validation apply

--- front/src/screens/auth/AuthHomeScreen.tsx
+++ front/src/screens/auth/AuthHomeScreen.tsx
@@ -1,21 +1,38 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {View, Text, StyleSheet, Button, SafeAreaView} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  SafeAreaView,
+  Image,
+  Dimensions,
+} from 'react-native';
 import {RootStackParamList} from '../../navigations/stack/AuthStackNavigator';
-import {authNavigations} from '../../constants';
+import {authNavigations} from '../../constants/navigations';
+import CustomButton from '../../components/CustomButton';
 
 type AuthHomeScreenProps = StackScreenProps<RootStackParamList>;
 
 const AuthHomeScreen = ({navigation}: AuthHomeScreenProps) => {
   return (
-    <SafeAreaView>
-      <View>
-        <Button
-          title="로그인 화면으로 이동"
+    <SafeAreaView style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          resizeMode="contain"
+          style={styles.image}
+          source={require('../../assets/MATZIP.png')}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <CustomButton
+          label="로그인 하기"
           onPress={() => navigation.navigate(authNavigations.LOGIN)}
         />
-        <Button
-          title="회원가입 화면으로 이동"
+        <CustomButton
+          variant="outlined"
+          label="회원가입 하기"
           onPress={() => navigation.navigate(authNavigations.SIGN_UP)}
         />
       </View>
@@ -23,6 +40,24 @@ const AuthHomeScreen = ({navigation}: AuthHomeScreenProps) => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    flex: 1,
+    gap: 10,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  imageContainer: {
+    flex: 1.5,
+    width: Dimensions.get('screen').width / 2,
+  },
+});
 
 export default AuthHomeScreen;
--- front/src/screens/auth/LoginScreen.tsx
+++ front/src/screens/auth/LoginScreen.tsx
@@ -1,14 +1,57 @@
-import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Button, SafeAreaView} from 'react-native';
+import InputField from '../../components/InputField';
+import CustomButton from '../../components/CustomButton';
+import useForm from '../../hooks/useForm';
+import {validateLogin} from '../../utils';
 
 const LoginScreen = () => {
+  const login = useForm({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validate: validateLogin,
+  });
+  console.log('getTextInputProps', login.getTextInputProps('email'));
+
+  const handlerSubmit = () => {
+    console.log('values', login.values);
+  };
+
   return (
-    <View>
-      <Text>로그인 스크린</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField
+          placeholder="아이디"
+          error={login.errors.email}
+          inputMode="email"
+          touched={login.touched.email}
+          {...login.getTextInputProps('email')}
+        />
+        <InputField
+          placeholder="비밀번호"
+          error={login.errors.password}
+          inputMode="numeric"
+          secureTextEntry
+          touched={login.touched.password}
+          {...login.getTextInputProps('password')}
+        />
+      </View>
+      <CustomButton label="로그인" onPress={handlerSubmit} />
+    </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    margin: 30,
+    flex: 1,
+  },
+  inputContainer: {
+    gap: 20,
+    marginBottom: 30,
+  },
+});
 
 export default LoginScreen;
