27dbee9ed85ee2182e5149f7ce42dc6b9198861c
chosule <89799325+chosule@users.noreply.github.com>
2025-04-09 21:56:23 +0900

    constans add

---
+++ front/src/constants/colors.ts
@@ -0,0 +1,13 @@
+const colors = {
+  WHITE: '#fff',
+  PINK_500: '#BF5C79',
+  PINK_700: '#C63B64',
+  RED_300: '#FFB4B4',
+  RED_500: '#ff5f5f',
+  GRAY_200: '#E7E7E7',
+  GRAY_500: '#8e8e8e',
+  GRAY_700: '#6575757',
+  BLACK: '#000',
+};
+
+export {colors};
