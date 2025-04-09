6f0ed3054ae2adbbed4656cb11c4dba13f4fb770
chosule <89799325+chosule@users.noreply.github.com>
2025-04-10 02:07:47 +0900

    login , signup refactor

--- front/src/screens/auth/LoginScreen.tsx
+++ front/src/screens/auth/LoginScreen.tsx
@@ -1,11 +1,13 @@
-import React, {useState} from 'react';
+import React, {useRef} from 'react';
 import {View, Text, StyleSheet, Button, SafeAreaView} from 'react-native';
 import InputField from '../../components/InputField';
 import CustomButton from '../../components/CustomButton';
 import useForm from '../../hooks/useForm';
 import {validateLogin} from '../../utils';
+import {TextInput} from 'react-native-gesture-handler';
 
 const LoginScreen = () => {
+  const passwordRef = useRef<TextInput | null>(null);
   const login = useForm({
     initialValues: {
       email: '',
@@ -22,17 +24,25 @@ const LoginScreen = () => {
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
         <InputField
+          autoFocus
           placeholder="아이디"
           error={login.errors.email}
           inputMode="email"
           touched={login.touched.email}
+          returnKeyType="next"
+          submitBehavior="newline"
+          onSubmitEditing={() => passwordRef.current?.focus()}
           {...login.getTextInputProps('email')}
         />
         <InputField
+          ref={passwordRef}
           placeholder="비밀번호"
           error={login.errors.password}
           inputMode="numeric"
-          secureTextEntry
+          secureTextEntry //비밀번호는 안보여야하니까
+          returnKeyType="join"
+          submitBehavior="newline"
+          onSubmitEditing={handlerSubmit}
           touched={login.touched.password}
           {...login.getTextInputProps('password')}
         />
--- front/src/screens/auth/SignupScreen.tsx
+++ front/src/screens/auth/SignupScreen.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
 import InputField from '../../components/InputField';
 import useForm from '../../hooks/useForm';
 import CustomButton from '../../components/CustomButton';
 import {validateSignup} from '../../utils';
+import {TextInput} from 'react-native-gesture-handler';
 
 const SignupScreen = () => {
   const signUp = useForm({
@@ -14,35 +15,52 @@ const SignupScreen = () => {
     },
     validate: validateSignup,
   });
+  const passwordRef = useRef<TextInput | null>(null);
+  const passwordConfirmRef = useRef<TextInput | null>(null);
 
+  const handleSubmit = () => {
+    console.log('회원가입 submit', signUp.values);
+  };
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
         <InputField
+          autoFocus
           placeholder="아이디"
           error={signUp.errors.email}
           inputMode="email"
           touched={signUp.touched.email}
+          returnKeyType="next"
+          submitBehavior="newline"
+          multiline={false}
+          onSubmitEditing={() => passwordRef.current?.focus()} //next를 누를고 나서 동작 적용
           {...signUp.getTextInputProps('email')}
         />
         <InputField
+          ref={passwordRef}
           placeholder="비밀번호"
           error={signUp.errors.password}
           inputMode="numeric"
           secureTextEntry
           touched={signUp.touched.password}
+          returnKeyType="next"
+          submitBehavior="newline"
+          onSubmitEditing={() => passwordConfirmRef.current?.focus()}
           {...signUp.getTextInputProps('password')}
         />
         <InputField
+          ref={passwordConfirmRef}
           placeholder="비밀번호 확인"
           error={signUp.errors.passwordConfirm}
           inputMode="numeric"
           secureTextEntry
           touched={signUp.touched.passwordConfirm}
+          returnKeyType="next"
+          onSubmitEditing={handleSubmit}
           {...signUp.getTextInputProps('passwordConfirm')}
         />
       </View>
-      <CustomButton label="회원가입" />
+      <CustomButton label="회원가입" onPress={handleSubmit} />
     </SafeAreaView>
   );
 };
